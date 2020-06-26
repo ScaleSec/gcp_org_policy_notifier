1abb4ddc6c7b40026c30bee20af13f4804c3c05b
jdyke <jasonadyke@gmail.com>
2020-06-26 17:25:08 -0400

    Slack functionality

--- main.tf
+++ main.tf
@@ -90,5 +90,8 @@ module "pubsub_scheduled_example" {
     FILE_LOCATION = var.file_location
     POLICY_FILE   = var.policy_file
     ORG_ID        = var.org_id
+    S_PROJECT     = var.secret_project
+    S_NAME        = var.secret_name
+    S_VERSION     = var.secret_version
   }
 }
