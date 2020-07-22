632b91f06df7d5252b45ad26820f37cbba2cb123
Chris Leibl <christopher@Chriss-MacBook-Pro.local>
2020-07-22 16:43:58 -0400

    Update Prefix to remove opn and sort vars by required

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "google-beta" {
 # Naming                 #
 #------------------------#
 locals {
-  name_prefix = "${var.name_prefix}-opn"
+  name_prefix = "${var.name_prefix}"
 }
 
 resource "random_id" "random" {
